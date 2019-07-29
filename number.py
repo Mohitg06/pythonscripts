a4dbe7a7df29ba9bb8acb456078d3dd4e9883e61
Mohit Gadkari <mohit.gadkari@teradata.com>
2019-07-28 22:03:15 -0700

    HackerRank Sample Test

--- number.py
+++ number.py
@@ -14,23 +14,23 @@ def findNumber(arr, k):
 findNumber(arr, k)
 
 
-# if __name__ == '__main__':
-#     fptr = open(os.environ['OUTPUT_PATH'], 'w')
-#
-#     arr_count = int(input().strip())
-#
-#     arr = []
-#
-#     for _ in range(arr_count):
-#         arr_item = int(input().strip())
-#         arr.append(arr_item)
-#
-#     k = int(input().strip())
-#
-#     res = findNumber(arr, k)
-#
-#     fptr.write(res + '\n')
-#
-#     fptr.close()
-#
-#
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = []
+
+    for _ in range(arr_count):
+        arr_item = int(input().strip())
+        arr.append(arr_item)
+
+    k = int(input().strip())
+
+    res = findNumber(arr, k)
+
+    fptr.write(res + '\n')
+
+    fptr.close()
+
+
